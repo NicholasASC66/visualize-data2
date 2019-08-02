b5dae9d8ef15cfed031533cbb6006f58a375c5a8
NicholasASC66 <crawfordn20@kent-schol.edu>
2019-08-02 15:03:37 -0400

    Co-authored-by: EricASC6 <ericchen9025@gmail.com>

---
+++ SRC/data.js
@@ -0,0 +1,273 @@
+const data = {
+    "results": [
+    {
+    "gender": "female",
+    "name": {
+    "title": "mrs",
+    "first": "laurie",
+    "last": "gauthier"
+    },
+    "location": {
+    "street": "6551 22nd ave",
+    "city": "summerside",
+    "state": "saskatchewan",
+    "postcode": "C9H 3T1",
+    "coordinates": {
+    "latitude": "-59.6309",
+    "longitude": "85.9672"
+    },
+    "timezone": {
+    "offset": "+5:45",
+    "description": "Kathmandu"
+    }
+    },
+    "email": "laurie.gauthier@example.com",
+    "login": {
+    "uuid": "a58ba0de-2888-4798-8613-1748fa6b8581",
+    "username": "brownbird538",
+    "password": "banger",
+    "salt": "GGGDJIFA",
+    "md5": "0f633cc223a4a5405c53dadefb151955",
+    "sha1": "f26faca3aeca45c83f829ddea3db9091767218c5",
+    "sha256": "2d40d680d2a1281730602fe4c4489d35f34700e4a40b09305bf1b2e327b96155"
+    },
+    "dob": {
+    "date": "1959-08-17T13:16:36Z",
+    "age": 59
+    },
+    "registered": {
+    "date": "2010-08-25T17:56:22Z",
+    "age": 8
+    },
+    "phone": "647-535-0711",
+    "cell": "185-422-0532",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/94.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/94.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/94.jpg"
+    },
+    "nat": "CA"
+    },
+    {
+    "gender": "female",
+    "name": {
+    "title": "ms",
+    "first": "lyna",
+    "last": "gauthier"
+    },
+    "location": {
+    "street": "8096 rue de la gare",
+    "city": "strasbourg",
+    "state": "indre-et-loire",
+    "postcode": 29574,
+    "coordinates": {
+    "latitude": "52.9213",
+    "longitude": "-132.3480"
+    },
+    "timezone": {
+    "offset": "-8:00",
+    "description": "Pacific Time (US & Canada)"
+    }
+    },
+    "email": "lyna.gauthier@example.com",
+    "login": {
+    "uuid": "21e96430-8531-4f44-828d-a5d2cae83708",
+    "username": "goldenpanda475",
+    "password": "pelican",
+    "salt": "CdGfI391",
+    "md5": "8af8b89bd5c8244d4a36b790877d4852",
+    "sha1": "aa3261734e49053d2f9f5d3e24f81280bd894100",
+    "sha256": "32c018446b550f144896fd957cbe527eb68905b2023a8a3fc17af28f53f9c824"
+    },
+    "dob": {
+    "date": "1991-01-04T09:20:53Z",
+    "age": 28
+    },
+    "registered": {
+    "date": "2004-10-13T20:23:56Z",
+    "age": 14
+    },
+    "phone": "01-57-42-14-77",
+    "cell": "06-46-73-74-69",
+    "id": {
+    "name": "INSEE",
+    "value": "2NNaN70006018 32"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/89.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/89.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/89.jpg"
+    },
+    "nat": "FR"
+    },
+    {
+    "gender": "female",
+    "name": {
+    "title": "ms",
+    "first": "thea",
+    "last": "arens"
+    },
+    "location": {
+    "street": "rosenweg 176",
+    "city": "erwitte",
+    "state": "thüringen",
+    "postcode": 60283,
+    "coordinates": {
+    "latitude": "-75.1856",
+    "longitude": "21.7435"
+    },
+    "timezone": {
+    "offset": "+10:00",
+    "description": "Eastern Australia, Guam, Vladivostok"
+    }
+    },
+    "email": "thea.arens@example.com",
+    "login": {
+    "uuid": "efda5b6e-2e28-4c74-b7cb-7ab5c7afc100",
+    "username": "silverostrich928",
+    "password": "camaro",
+    "salt": "S420OsUl",
+    "md5": "adda1dc988f1f88648d958f2f62e16de",
+    "sha1": "897ccfa9e3a8cdd4a8909ca5cfec55edeba7049a",
+    "sha256": "ce7adc1dcb265423a8d480023d802166cf0e20dc0141977d44656d531d9b2997"
+    },
+    "dob": {
+    "date": "1988-10-15T15:09:25Z",
+    "age": 30
+    },
+    "registered": {
+    "date": "2016-02-13T07:46:31Z",
+    "age": 3
+    },
+    "phone": "0244-3903956",
+    "cell": "0178-5788609",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/11.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/11.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/11.jpg"
+    },
+    "nat": "DE"
+    },
+    {
+    "gender": "male",
+    "name": {
+    "title": "mr",
+    "first": "santiago",
+    "last": "navarro"
+    },
+    "location": {
+    "street": "4331 calle de téllez",
+    "city": "madrid",
+    "state": "castilla y león",
+    "postcode": 35067,
+    "coordinates": {
+    "latitude": "24.0368",
+    "longitude": "-159.5215"
+    },
+    "timezone": {
+    "offset": "+8:00",
+    "description": "Beijing, Perth, Singapore, Hong Kong"
+    }
+    },
+    "email": "santiago.navarro@example.com",
+    "login": {
+    "uuid": "2d5549e0-df0d-43f8-bedf-a4c23baedd14",
+    "username": "bluedog389",
+    "password": "scruffy",
+    "salt": "oVAcYC6T",
+    "md5": "c345a9edcab44ed9b8fba22d94f48d07",
+    "sha1": "50af06885c123798cb5683a4510f5d63a22da225",
+    "sha256": "2023904027de3e13f95c842d79e2c7730727cfe6c8f976b8b1cc9ec3606f3812"
+    },
+    "dob": {
+    "date": "1984-08-15T04:00:32Z",
+    "age": 34
+    },
+    "registered": {
+    "date": "2017-05-19T13:22:43Z",
+    "age": 2
+    },
+    "phone": "927-790-372",
+    "cell": "644-657-650",
+    "id": {
+    "name": "DNI",
+    "value": "06269772-Z"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/51.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/51.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/51.jpg"
+    },
+    "nat": "ES"
+    },
+    {
+    "gender": "male",
+    "name": {
+    "title": "mr",
+    "first": "robert",
+    "last": "tucker"
+    },
+    "location": {
+    "street": "581 albert road",
+    "city": "winchester",
+    "state": "lancashire",
+    "postcode": "G2P 9NF",
+    "coordinates": {
+    "latitude": "-16.4599",
+    "longitude": "-31.6783"
+    },
+    "timezone": {
+    "offset": "+5:00",
+    "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+    }
+    },
+    "email": "robert.tucker@example.com",
+    "login": {
+    "uuid": "5f14f89d-8bce-4a0f-a52e-ad6a8543c60d",
+    "username": "blackbear589",
+    "password": "zelda",
+    "salt": "v1YUn1lV",
+    "md5": "ad4b8069909453c1efd4605111635532",
+    "sha1": "cdf38e60c8c3a25a8377e78ba64c00c47e803630",
+    "sha256": "001d1bf3d0f97e8732a946f0fd75413ec1304fe1a074e15810ddf66c6ae0b36c"
+    },
+    "dob": {
+    "date": "1948-12-04T14:10:24Z",
+    "age": 70
+    },
+    "registered": {
+    "date": "2012-09-28T13:26:46Z",
+    "age": 6
+    },
+    "phone": "015396 74561",
+    "cell": "0771-141-923",
+    "id": {
+    "name": "NINO",
+    "value": "EG 37 77 57 W"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/66.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/66.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/66.jpg"
+    },
+    "nat": "GB"
+    }
+    ],
+    "info": {
+    "seed": "9a5398081077e736",
+    "results": 5,
+    "page": 1,
+    "version": "1.2"
+    }
+    }
+
+    const list = data.results;
+    console.log(list)
---
+++ SRC/main.js
@@ -0,0 +1,67 @@
+
+const main= document.querySelector("main")
+
+const createCard = (obj,indx) =>{
+    //card that contains user info
+    const newCard = document.createElement("div")
+    newCard.className = "card"
+    newCard.id= indx;
+
+//Face of user
+    const face = document.createElement('img')
+    face.src = obj.picture.medium
+    face.alt = "Portrait"
+
+// div that contains name, age and email
+    const info = document.createElement("div")
+    info.className = "info"
+    const lst = document.createElement("ul")
+    const name = document.createElement('li')
+    const email = document.createElement('li')
+    const age = document.createElement('li')
+    name.innerHTML= `Name: ${title(list[indx].name.first)}  ${title(list[indx].name.last)}`
+    email.innerHTML = `Email: ${list[indx].email}`
+    age.innerHTML= `Age: ${list[indx].dob.age}`
+
+    lst.appendChild(name)
+    lst.appendChild(age)
+    lst.appendChild(email)
+    newCard.appendChild(face);
+    newCard.appendChild(lst)
+    main.appendChild(newCard);
+}
+
+
+const loopCreate=arr=>{
+    for(let i = 0; i<arr.length; i++){
+    createCard(arr[i],i);
+    }
+}
+
+const title = name => {
+    nameArr = name.trim().split(" ");
+  
+    arr = [];
+    for (let word of nameArr) {
+      arr.push([...word]);
+    }
+  
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (j === 0) {
+          let l = arr[i].shift();
+          arr[i].unshift(l.toUpperCase());
+        }
+      }
+    }
+  
+    titleName = ""
+    for (const word of arr) {
+      titleName += " " +word.join("");
+    }
+  
+    return titleName.trim()
+  }
+
+loopCreate(list);
+
